fef70b6517fe75cf0bfa859bddba211c7f03c03f
p-lisa <lisa.peereman@gmail.com>
2026-04-30 23:25:59 +0200

    Add state imgs

--- src/assets/assets.h
+++ src/assets/assets.h
@@ -2,10 +2,10 @@
 #include <stdint.h> // incl standard header that defines fixed width int type
 
 extern const uint16_t sleep_img[];
-// extern const uint16_t active_img[];
-
+extern const uint16_t active_img[];
+extern const uint16_t eat_img[];
 extern const uint16_t loot_img[];
-// extern const uint16_t a_execute_img[];
+extern const uint16_t execute_img[];
 
 // extern const uint16_t* a_eat_imgs[];
 // extern const int eat_frame_count;
